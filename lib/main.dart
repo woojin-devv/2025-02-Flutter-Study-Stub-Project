4cbfa17780c664ee28567bca68851900020080b1
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-11-24 13:58:20 +0900

    Squashed '2025-02-flutter-stub/xylophone/' content from commit 15b96da
    git-subtree-dir: 2025-02-flutter-stub/xylophone
    git-subtree-split: 15b96da374f16d241ec0dc586488afbfca508539

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Container()));
+  }
+}
